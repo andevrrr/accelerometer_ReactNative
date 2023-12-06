8e831bfaf03b89be805767db3d24747c491ab430
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-06 17:06:21 +0300

    usage of Platform.OS

--- App.js
+++ App.js
@@ -1,17 +1,61 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, Text, Button, StyleSheet, Platform, Alert } from "react-native";
 
 const App = () => {
+  const openPlatformSpecificComponent = () => {
+    if (Platform.OS === "android") {
+      openAndroidModal();
+    } else if (Platform.OS === "ios") {
+      openIosModal();
+    }
+  };
+
+  const openAndroidModal = () => {
+    Alert.alert(
+      "Android Modal",
+      "This is an Android-specific modal.",
+      [
+        { text: "Option 1", onPress: () => console.log("Option 1 selected") },
+        { text: "Option 2", onPress: () => console.log("Option 2 selected") },
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel pressed"),
+          style: "cancel",
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const openIosModal = () => {
+    Alert.alert(
+      "iOS Modal",
+      "This is an iOS-specific modal.",
+      [
+        { text: "Option 1", onPress: () => console.log("Option 1 selected") },
+        { text: "Option 2", onPress: () => console.log("Option 2 selected") },
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel pressed"),
+          style: "cancel",
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={[styles.section, { flex: 20, backgroundColor: "pink" }]}>
-        <Text>Header</Text>
+        <Text style={styles.headerText}>Platform-Specific Example</Text>
       </View>
 
       <View style={[styles.section, { flex: 60, backgroundColor: "white" }]}>
-        <View style={[styles.box, { backgroundColor: "yellow" }]} />
-        <View style={[styles.box, { backgroundColor: "green" }]} />
-        <View style={[styles.box, { backgroundColor: "red" }]} />
+        <Text style={styles.text}>This is common text for both platforms.</Text>
+        <Button
+          title="Open Platform Component"
+          onPress={openPlatformSpecificComponent}
+        />
       </View>
 
       <View style={[styles.section, { flex: 20, backgroundColor: "gray" }]}>
@@ -30,9 +74,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  box: {
-    width: "33.33%",
-    aspectRatio: 1,
+  headerText: {
+    fontSize: 18,
+    color: "white",
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 20,
   },
 });
 
